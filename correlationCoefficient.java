f76bdf4617c3f138fc1f4de3dbb67e1a36260767
gillarious <isabella.berryx3@gmail.com>
2018-09-20 13:39:43 -0400

    adding values to array

--- correlationCoefficient.java
+++ correlationCoefficient.java
@@ -6,7 +6,9 @@ public class correlationCoefficient {
         Scanner sc = new Scanner(System.in);
         double sigLevel = 0;
         int size = 10;
-        String input = ""; 
+        String input = "";
+        String xVal = "";
+        String yVal = "";
         double[] xArray = {0,0,0,0,0,0,0,0,0,0};
         double[] yArray = {0,0,0,0,0,0,0,0,0,0};
         double criticalValue = 0;
@@ -20,7 +22,16 @@ public class correlationCoefficient {
             System.out.println("Enter the value for x and y array: ");
             input = sc.nextLine();
             
+            input = input.replaceAll(" ","");
+            xval = input.subString(0,1);
+            yval = input.subString(2,3);
+
+            xArray[i] = double(int(xval));
+            yArray[i] = double(int(yval));
         }
+
+        System.out.println(xArray);
+        System.out.println(yArray);
 
         System.out.println("Critical value is equal to: " + criticalValue);
         System.out.println("Linear correlation coefficient is equal to: " + linearCorrelationCoefficient);
